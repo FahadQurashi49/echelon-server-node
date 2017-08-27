39ad493c7755a4ec27c370067f098da29f3995c3
FahadQurashi49 <fahad.ahmad049@gmail.com>
2017-08-27 20:25:54 +0500

    add facility service

--- routes/facility_routes.js
+++ routes/facility_routes.js
@@ -1,47 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const Facility = require('../models/facility');
+const facilityService = require('../services/facility_service');
 
 router.get('/facilities/:id', function (req, res, next) {
-  Facility.findById(req.params.id).then(function(facility) {
-    res.json(facility);
-  }).catch(next);
+  facilityService.getFacility(req, res, next);
 });
 
 router.get('/facilities', function (req, res, next){
-  var pageOptions = {
-    page: req.query.page || 0,
-    limit: req.query.limit || 10
-  };
-  Facility.find()
-    .skip(pageOptions.page*pageOptions.limit)
-    .limit(pageOptions.limit)
-    .exec().then(function (facility) {
-      res.json(facility);
-    }).catch(next);
-
+  facilityService.getAllFacilities(req, res, next);
 });
 // add a new facility
 router.post('/facilities', function (req, res, next){
-  Facility.create(req.body).then(function(facility) {
-    res.json(facility);
-  }).catch(next);
+  facilityService.addFacility(req, res, next);
 });
 
 //update a facility in db by ID
 router.put('/facilities/:id', function (req, res, next) {
-  Facility.findByIdAndUpdate({_id: req.params.id}, req.body).then(function (){
-    Facility.findOne({_id: req.params.id}).then(function (facility) {
-      res.json(facility);
-    });
-  }).catch(next);
+  facilityService.updateFacility(req, res, next);
 });
 
 //delete a facility from db by ID
 router.delete('/facilities/:id', function (req, res, next) {
-  Facility.findByIdAndRemove({_id: req.params.id}).then(function (facility) {
-    res.json(facility);
-  }).catch(next);
+  facilityService.deleteFacility(req, res, next);
 });
 
 module.exports = router;
---
+++ services/facility_service.js
@@ -0,0 +1,54 @@
+const Facility = require('../models/facility');
+const facilityException = require('../exception/facility_exceptions');
+const PageOptions = require('./page_options');
+
+function FacilityService() {
+
+}
+// get facility by ID
+FacilityService.prototype.getFacility = function (req, res, next) {
+    Facility.findById(req.params.id).then(function (facility) {
+        facilityException.facilityNotFound(facility);
+        res.json(facility);
+    }).catch(next);
+}
+
+// get all facilities
+FacilityService.prototype.getAllFacilities = function (req, res, next) {
+    var pageOptions = new PageOptions(req);
+    Facility.find()
+        .skip(pageOptions.page)
+        .limit(pageOptions.limit)
+        .exec().then(function (facilities) {
+            res.json(facilities);
+        }).catch(next);
+}
+
+// add a new facility
+FacilityService.prototype.addFacility = function (req, res, next) {
+    Facility.create(req.body).then(function(facility) {
+        res.json(facility);
+      }).catch(next);
+}
+
+// update a facility by ID
+FacilityService.prototype.updateFacility = function (req, res, next) {
+    Facility.findOneAndUpdate({_id: req.params.id}, req.body).then(function (){
+        Facility.findOne({_id: req.params.id}).then(function (facility) {
+            facilityException.facilityNotFound(facility);
+            res.json(facility);
+        }).catch(next);
+      }).catch(next);
+}
+
+// delete a facility by ID
+FacilityService.prototype.deleteFacility = function (req, res, next) {
+    Facility.findByIdAndRemove({_id: req.params.id}).then(function (facility) {
+        facilityException.facilityNotFound(facility);
+        res.json(facility);
+      }).catch(next);
+}
+
+var facilityService = new FacilityService();
+
+module.exports = facilityService;
